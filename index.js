b0c9411059a08d2f3ea336bdc0e3f7ed7d751bff
Mokhlaroyim <mokhlaroyim097@gmail.com>
2020-11-13 22:19:34 +0500

    sample

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ function initMap() {
     zoom: 8,
   });
 }
+
+var getClassLeft = document.getElementById("links-left");
+var getClassRight = document.getElementById("links-right");
+var getAnchor1 = getClassLeft.getElementsByTagName('a');
+var getAnchor2 = getClassRight.getElementsByTagName('a');
+
+function myFunction(x) {
+
+  if(x.classList.toggle("change")){
+    getClassLeft.style.display = 'contents';
+    getClassRight.style.display = 'contents';
+    for(let i=0; i<getAnchor1.length; i++){
+      getAnchor1[i].style.display = 'block';
+      getAnchor2[i].style.display = 'block';
+    }
+  } 
+  else{
+    getClassLeft.style.display = 'none';
+    getClassRight.style.display = 'none';
+  }   
+}
